1bedb55760e69485ca046661cbb7909e2c743b23
Karen Freeman-Smith and Sara Jensen <karen@>
2016-11-15 11:13:23 -0800

    create story

--- app/src/main/java/com/example/guest/madlibs/MainActivity.java
+++ app/src/main/java/com/example/guest/madlibs/MainActivity.java
@@ -5,12 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.button) Button mButton;
+    @Bind(R.id.inputBodyPartEditText) EditText mBodyPartEditText;
+    @Bind(R.id.inputDayEditText) EditText mDayEditText;
+    @Bind(R.id.inputFacialExpressionEditText) EditText mExpressionEditText;
+    @Bind(R.id.inputFoodEditText) EditText mFoodEditText;
+    @Bind(R.id.inputFoodPlacEditText) EditText mFoodPlaceEditText;
+    @Bind(R.id.inputNounEditText) EditText mNounEditText;
+    @Bind(R.id.inputVehicleEditText) EditText mVehicleEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +30,27 @@ public class MainActivity extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String vehicle = mVehicleEditText.getText().toString();
+                String noun = mNounEditText.getText().toString();
+                String place = mFoodPlaceEditText.getText().toString();
+                String food = mFoodEditText.getText().toString();
+                String expression = mExpressionEditText.getText().toString();
+                String day = mDayEditText.getText().toString();
+                String bodypart = mBodyPartEditText.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+
+                intent.putExtra("vehicle", vehicle);
+                intent.putExtra("noun", noun);
+                intent.putExtra("place", place);
+                intent.putExtra("food", food);
+                intent.putExtra("expression", expression);
+                intent.putExtra("day", day);
+                intent.putExtra("bodypart", bodypart);
+
                 startActivity(intent);
+
+
             }
         });
     }
--- app/src/main/java/com/example/guest/madlibs/StoryActivity.java
+++ app/src/main/java/com/example/guest/madlibs/StoryActivity.java
@@ -1,13 +1,46 @@
 package com.example.guest.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class StoryActivity extends AppCompatActivity {
+    @Bind(R.id.storyTextView) TextView mStoryTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String vehicle = intent.getStringExtra("vehicle");
+        String food = intent.getStringExtra("food");
+        String bodypart = intent.getStringExtra("bodypart");
+        String expression = intent.getStringExtra("expression");
+        String noun = intent.getStringExtra("noun");
+        String place = intent.getStringExtra("place");
+        String day = intent.getStringExtra("day");
+
+        Toast.makeText(StoryActivity.this, vehicle, Toast.LENGTH_SHORT).show();
+
+        String story = "She tripped on a " + food;
+        story += " and fell into the street in front of a " + vehicle + ".";
+        story += " The " + vehicle + " driver screeched the " + vehicle + " to a halt and yelled, 'How dare you!' \n\n";
+        story += "She was so mad that she picked up the smooshed " + food + " and threw it at the driver's " + bodypart;
+        story += ". The driver, little did she know, loved her spunk and " + expression;
+        story += ". The " + food + " guts ran down his brow and onto his " + bodypart;
+        story += ". She said, 'you've got " + food + " on your " + bodypart;
+        story += ". haha, haha.' \n\n";
+        story += "He wiped his " + bodypart + " and asked, 'Are you free " + day + " night?'";
+        story += " Shocked, but not surprised, she replied, 'Normally I wouldn't say yes but you've got a great " + noun + ". ";
+        story += "He replied, 'Good, " + place + " is a great place for a food fight.'";
+
+        mStoryTextView.setText(story);
     }
 }
