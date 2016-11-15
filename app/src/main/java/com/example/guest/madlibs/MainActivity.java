c0b49b439f19fde24e30ce4966153c530fa69c45
Karen Freeman-Smith and Sara Jensen <sara@>
2016-11-15 09:59:20 -0800

    begin android project; add form fields, add button that goes to second page; WIP

---
+++ app/src/main/java/com/example/guest/madlibs/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.guest.madlibs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.button) Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
